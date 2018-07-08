c6dd9c0236ffb883e2be25a391b86594dc762e11
RobijnDM <henriques.gabriel@outlook.com>
2018-07-08 06:05:13 -0300

    Finishing up routes

--- routes/todos.js
+++ routes/todos.js
@@ -17,7 +17,34 @@ router.post('/', (req,res) => {
   }).catch((err) => {
     res.send(err);
   });
+});
+
+router.get('/:todoId', (req,res) => {
+  db.Todo.findById(req.params.todoId)
+  .then((todo) => {
+    res.json(todo);
+  }).catch((err) => {
+    res.send(err);
+  })
+});
+
+router.put('/:todoId', (req,res) => {
+  db.Todo.findByIdAndUpdate({_id: req.params.todoId}, req.body, {new:true})
+  .then((todo) => {
+    res.json(todo);
+  }).catch((err) => {
+    res.send(err)
+  })
+});
+
+router.delete('/:todoId', (req,res) => {
+  db.Todo.remove({_id: req.params.todoId})
+  .then((todo) => {
+    res.json({message: 'We deleted it!'})
+  }).catch((err) => {
+    res.send(err)
+  })
 })
 
-
 module.exports = router
+
