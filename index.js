fb96d4ee3a6988dd95d25e615a383f66bf547bce
RobijnDM <henriques.gabriel@outlook.com>
2018-07-10 12:34:05 -0300

    testing CI in windows

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.use('/api/todos', todoRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+  console.log("/n Server started on port " + PORT);
 })
